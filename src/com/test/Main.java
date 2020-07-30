f99bcfd94072045b3d619f165cfe97cf76e191fb
zhumingzhou <zhumzhou1996@163.com>
2020-07-30 11:41:20 +0800

    zmzzmz

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -1,47 +1,31 @@
 package com.test;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = {10,5,8,20,5,3,54,12,69,87,45,21,1,3,5};
-        new Main().sort(arr,0,arr.length-1);
-        System.out.println(Arrays.toString(arr));
-    }
-    public void sort(int[] a, int low, int high) {
-        int start = low;
-        int end = high;
-        int key = a[low];
-        while (end > start) {
-            //从后往前比较
-            while (end > start && a[end] >= key)
-
-                end--;
-            if (a[end] <= key) {
-                int temp = a[end];
-                a[end] = a[start];
-                a[start] = temp;
-            }
-
-            while (end > start && a[start] <= key)
-
-                start++;
-            if (a[start] >= key) {
-                int temp = a[start];
-                a[start] = a[end];
-                a[end] = temp;
-            }
-
-
+        // 创建一个key和value均为String的Map集合
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("1", "11");
+        map.put("2", "22");
+        map.put("3", "33");
+        // 键和值
+        String key = null;
+        String value = null;
+        // 获取键值对的迭代器
+        Iterator it = map.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry entry = (Map.Entry) it.next();
+            key = (String) entry.getKey();
+            value = (String) entry.getValue();
+            System.out.println("key:" + key + "---" + "value:" + value);
         }
-        //递归
-        if (start > low) sort(a, low, start - 1);//左边序列。第一个索引位置到关键值索引-1
-        if (end < high) sort(a, end + 1, high);//右边序列。从关键值索引+1 到最后一个
+
     }
+
+
 }
 
 
