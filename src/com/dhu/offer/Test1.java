1ae40827980012f35588083fd7a63ddfe3c27e32
zhumzhou <zhumzhou1996@163.com>
2020-09-27 22:41:08 +0800

    默认更改列表

--- src/com/dhu/offer/Test1.java
+++ src/com/dhu/offer/Test1.java
@@ -1,7 +1,42 @@
 package com.dhu.offer;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("zaksjkadhjkahdgk");
+
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        str = str.replace("[", "");
+        str = str.replace("]","");
+        String[] spl = str.split(",");
+        int[] arr = new int[spl.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=Integer.parseInt(spl[i]);
+        }
+
+        ArrayList<Integer> integers = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == 1 || arr[i] == 0) {
+
+            } else {
+                boolean isSushu = true; //标记
+                for (int j = 2; j < arr[i]; j++) {
+                    if (arr[i] % j == 0) {
+                        isSushu = false;
+                        break;
+                    }
+                }
+                if (isSushu == true) {
+                    integers.add(arr[i]);
+                }
+            }
+        }
+        String s = integers.toString();
+        s = s.replaceAll(" ", "");
+        System.out.println(s);
     }
 }
---
+++ src/com/dhu/offer/Test2.java
@@ -0,0 +1,25 @@
+package com.dhu.offer;
+
+import java.util.Scanner;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        str = str.replace("{", "");
+        str = str.replace("}","");
+        String[] spl = str.split(",");
+        int[][] arr = new int[spl.length][2];
+        for (int i = 0; i < arr.length; i++) {
+
+
+
+        }
+
+    }
+
+
+
+}
